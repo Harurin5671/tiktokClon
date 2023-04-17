46618e8c0c0ea7ea6f6d48a603ca13c32044e763
Haru <frankcito639@gmail.com>
2023-04-16 22:02:14 -0500

    chore(Root): añadiendo y ajustando los iconos

--- HaruTok/src/components/VideoPlayer/VideoPlayerActions.jsx
+++ HaruTok/src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,19 +1,24 @@
 import { Heart } from '../Icons/Heart.jsx'
+import { Comment } from '../Icons/Comment.jsx'
+import { Share } from '../Icons/Share.jsx'
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({ likes = 2041, comments = 333, shares = 50, hearted = false }) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='likes'>{likes}</strong>
       </div>
 
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title='comments'>{comments}</strong>
       </div>
 
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='shares'>{shares}</strong>
       </div>
 
     </aside>
