48674f2aef414e689c9ac854f7b13fff3aa32211
Haru <frankcito639@gmail.com>
2023-04-23 20:25:11 -0500

    agregando el hook

--- HaruTok/src/components/VideoPlayer/index.jsx
+++ HaruTok/src/components/VideoPlayer/index.jsx
@@ -1,20 +1,13 @@
 import clsx from 'clsx'
-import { useRef, useState } from 'react'
-import styles from './styles.module.css'
+import { useRef } from 'react'
 import VideoPlayerActions from './VideoPlayerActions.jsx'
 import VideoDescription from '../VideoDescription/index.jsx'
+import useIntersectionVideoPlayer from '../../hooks/useIntersectionVideoPlayer.js'
+import styles from './styles.module.css'
 
 export default function VideoPlayer ({ username, avatar, albumImage, description, songTitle, src }) {
-  const [playing, setPlaying] = useState(false)
-
   const video = useRef(null)
-
-  const handlePlay = () => {
-    const { current: videoEl } = video
-    playing ? videoEl.pause() : videoEl.play()
-
-    setPlaying(!playing)
-  }
+  const { playing, handlePlay } = useIntersectionVideoPlayer({ video })
 
   const playerClassName = clsx(styles.player, {
     [styles.hidden]: playing
